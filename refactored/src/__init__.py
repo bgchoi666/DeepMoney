3af9beda33f3a904c3be0f0b882186499c9f3be2
bgchoi666 <34165682+bgchoi666@users.noreply.github.com>
2026-02-21 10:55:29 +0900

    Delete refactored directory

--- refactored/src/__init__.py
+++
@@ -1,5 +0,0 @@
-"""DeepMoney v2 - src 패키지"""
-from src.config_loader import Config, load_config
-from src.data_reader import IndexDataset
-from src.model import DeepMoneyModel, build_model
-from src.trainer import Trainer, Predictor
